63085b825ac1913e5324ecf30e61e291327f93f8
Mohan Liu <mohan.liu.business@gmail.com>
2019-08-21 12:58:03 +0800

    feat(conway): add conway algo

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.pattern = init();
+app.users = {};
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +38,34 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function init() {
+  let cells = [];
+  for (var i = 0; i < 64; i++) {
+    cells[i] = [];
+    for (var j = 0; j < 64; j++) {
+      cells[i][j] = 0;
+    }
+  }
+
+  // Prefilled cells
+  [
+    // Gosper glider gun
+    [1, 5], [1, 6], [2, 5], [2, 6], [11, 5], [11, 6], [11, 7], [12, 4], [12, 8], [13, 3], [13, 9], [14, 3], [14, 9], [15, 6], [16, 4], [16, 8], [17, 5], [17, 6], [17, 7], [18, 6], [21, 3], [21, 4], [21, 5], [22, 3], [22, 4], [22, 5], [23, 2], [23, 6], [25, 1], [25, 2], [25, 6], [25, 7], [35, 3], [35, 4], [36, 3], [36, 4],
+
+    // Random cells
+    // If you wait enough time these will eventually take part
+    // in destroying the glider gun, and the simulation will be in a "static" state.
+    [60, 47], [61, 47], [62, 47],
+    [60, 48], [61, 48], [62, 48],
+    [60, 49], [61, 49], [62, 49],
+    [60, 51], [61, 51], [62, 51],
+  ]
+    .forEach(function (point) {
+      cells[point[0]][point[1]] = 1;
+    });
+
+  return cells;
+}
 
 module.exports = app;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,49 @@
-var socket = io();
+(function (window, document, undefined) {
+
+  // code that should be taken care of right away
+
+  window.onload = init;
+
+  function init() {
+
+    var socket = io();
+    let cells = [];
+    let myColor;
+
+    socket.on('updateCanvas', function (latestCells) {
+      cells = latestCells;
+      draw();
+    });
+
+    socket.on('yourColor', function (color) {
+      myColor = color;
+      console.log(myColor);
+      document.getElementsByClassName('square')[0].style.backgroundColor = myColor;
+    });
+
+    var canvas = document.getElementById('c').getContext('2d');
+    canvas.strokeStyle = '#e1e1e1';
+    canvas.fillStyle = 'cadetblue';
+
+    /**
+     * Draw cells on canvas
+     */
+    function draw() {
+      canvas.clearRect(0, 0, 1512, 512);
+      cells.forEach(function (row, x) {
+        row.forEach(function (cell, y) {
+          canvas.beginPath();
+          canvas.rect(x * 8, y * 8, 8, 8);
+          if (cell) {
+            canvas.fill();
+          } else {
+            canvas.stroke();
+          }
+        });
+      });
+      // setTimeout(function () { update(); }, 70);
+      //window.requestAnimationFrame(update); // Too fast!
+    }
+  }
+
+})(window, document, undefined);
